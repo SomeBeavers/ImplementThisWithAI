e620916db01e37465d3da8bbd74de7ce4bc1d17d
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-07-03 16:15:52 +0200

    Upd.

--- Core_App/Class1.cs
+++ Core_App/Class1.cs
@@ -3,17 +3,20 @@
 public class Class1 : IDisposable
 {
     private string _s;
+    delegate void Del(string message);
 
     public void Method1(int[] unsortedInts, List<int> sortedList)
     {
         Console.WriteLine("Core_App.Class1.Method1");
         var s = InsertMembersToSortedList(unsortedInts, sortedList);
-        
+
+
+        sortedList.Where(x => IsEven(x));
+        Del handler = A;
         new Class2(unsortedInts).Method2();
         new Class3().Method3();
         Console.WriteLine(s);
     }
-
 
     public void Dispose()
     {
