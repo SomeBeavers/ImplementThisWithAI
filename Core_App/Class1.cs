ca832df38bd8ea0e3e781b05738e0df97fcc9b15
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-07-03 10:14:35 +0200

    Add project files.

---
+++ Core_App/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Core_App;
+
+public class Class1 : IDisposable
+{
+    private string _s;
+
+    public void Method1(int[] unsortedInts, List<int> sortedList)
+    {
+        Console.WriteLine("Core_App.Class1.Method1");
+        var s = InsertMembersToSortedList(unsortedInts, sortedList);
+        
+        new Class2(unsortedInts).Method2();
+    }
+
+    public string S
+    {
+        get { }
+        set => _s = value;
+    }
+
+    public void Dispose()
+    {
+        // TODO release managed resources here
+    }
+}
---
+++ Core_App/Class2.cs
@@ -0,0 +1,22 @@
+﻿namespace Core_App;
+
+public interface IWorldMap<TNodeId, TRoadId>
+    where TNodeId : struct
+    where TRoadId : notnull
+{
+    void GetAttraction<TNodeId>(TNodeId it) where TNodeId : struct
+    {
+        // Implementation logic here
+        Console.WriteLine($"Getting attraction for node: {it}");
+    }
+}
+
+public class UseInterface<TNodeId, TRoadId> : IWorldMap<TNodeId, TRoadId>
+    where TNodeId : struct
+    where TRoadId : notnull
+{
+    public void Test(IWorldMap<TNodeId, TRoadId> map, TNodeId it)
+    {
+        map.GetAttraction(it);
+    }
+}
