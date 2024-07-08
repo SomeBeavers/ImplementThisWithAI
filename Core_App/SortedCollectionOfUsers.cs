03848220abb373aebb26281f803c756025b6c606
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-07-08 14:28:25 +0200

    Add indexer.

---
+++ Core_App/SortedCollectionOfUsers.cs
@@ -0,0 +1,6 @@
+﻿namespace Core_App;
+
+public class SortedCollectionOfUsers
+{
+    
+}
---
+++ Core_App/UseSortedCollectionOfUsers.cs
@@ -0,0 +1,19 @@
+﻿namespace Core_App;
+
+public class UseSortedCollectionOfUsers
+{
+    public User GetUserFromCollection(SortedCollectionOfUsers collection, int index)
+    {
+        if (collection == null)
+        {
+            throw new ArgumentNullException(nameof(collection));
+        }
+
+        if (index < 0 || index >= collection[index].Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        return collection[index];
+    }
+}
